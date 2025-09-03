cd579b96ad9716b48d87d4ce9eb93b7058b5081e
NIKHIl <unickk99@gmail.com>
2025-09-03 22:18:23 +0530

    adding python file for demo

---
+++ Credit_car_fraud_detection.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+# Install dependencies as needed:
+# pip install kagglehub[pandas-datasets]
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
